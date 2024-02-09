85e8b7bcbca00c0315f39de8df9374e5383d8119
sangjuneeeee <wns1169@gmail.com>
2024-02-09 20:55:52 +0900

    define colors

---
+++ WebGame_FE/src/styles/colors.stylex.ts
@@ -0,0 +1,12 @@
+import * as stylex from '@stylexjs/stylex';
+
+export const colors = stylex.defineVars({
+  p1: '#392467',
+  p2: '#5D3587',
+  p3: '#A367B1',
+  p4: '#FFD1E3',
+});
+
+/* 
+주의사항 : https://stylexjs.com/docs/learn/theming/defining-variables/
+*/
