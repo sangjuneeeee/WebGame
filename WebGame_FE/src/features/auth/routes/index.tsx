86487a01024ef7ab47a4659d7905f2a1ec9216e8
sangjuneeeee <wns1169@gmail.com>
2024-02-11 23:40:31 +0900

    add auth routes

--- WebGame_FE/src/features/auth/routes/index.tsx
+++ WebGame_FE/src/features/auth/routes/index.tsx
@@ -1,12 +1,12 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
-import { Login } from "./Login";
-// import { Register } from './Register';
+import { Login } from './Login';
+import { Register } from './Register';
 
 export const AuthRoutes = () => {
   return (
     <Routes>
-      {/* <Route path="register" element={<Register />} /> */}
+      <Route path="register" element={<Register />} />
       <Route path="login" element={<Login />} />
     </Routes>
   );
